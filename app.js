2c64352b51604f36279c27f270459fe537c317c9
humaysafarova <166644215+humaysafarova@users.noreply.github.com>
2025-06-04 11:30:16 +0000

    commit

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
 "use strict";
 $(document).ready(function () {
 
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=b4dea0c226eb5470b2ff752df032116d&units=metric`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myWeather").val("")
+                console.log(data)
+            }
+            myweather()
+        }
+    })
+
+
+    $("#btn").click(function (e) {
+        e.preventDefault()
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=b4dea0c226eb5470b2ff752df032116d&units=metric`
+        async function myweather() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels_like)
+            $("#myWeather").val("")
+            console.log(data)
+        }
+        myweather()
+    })
+
+
+
+
+
+
+
 });
